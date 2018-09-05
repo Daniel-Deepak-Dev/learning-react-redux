82a1f268b72b14c5a9fa21e0b208ee1dd7435db3
dana_tcoc <danatcoc94@gmail.com>
2018-09-06 01:57:54 +0530

    added videolist component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search'
 import { API_KEY } from './api';
-console.log(API_KEY);
+import VideoList from './components/video_list';
 
 
-const App = () => {
-    return <div><SearchBar/>s</div>;
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state={ videos:[] }
+        YTSearch({key:API_KEY,term:'mensutra'}, videos=>{
+            this.setState({videos});
+            //this.setState({videos/*name of key in state*/:videos/*data obtained from arg of the arrow function */})
+            // can rewrite videos:videos as videos
+        })
+}
+    render(){
+        return  <div>
+                    <SearchBar  />
+                    <hr/>
+                    <VideoList videoProp={this.state.videos}/>
+                </div>;
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
